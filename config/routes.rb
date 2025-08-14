e319ec622e25821afd7567051d3b081036f156c5
shattori <dev@DESKTOP-FBOEJDH>
2025-08-14 16:03:33 +0900

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "application#goodbye"
+  root "application#hello"
   # root "posts#index"
 end
